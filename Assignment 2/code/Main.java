25d04131ce6a39a9ecb9758d658e67ddeca064fd
Nicholas Rahbany <nrahbany22@yahoo.com>
2018-11-05 18:36:26 -0500

    Temp

--- Assignment 2/code/Main.java
+++ Assignment 2/code/Main.java
@@ -1,13 +1,37 @@
-// This will be used to run the empirical tests
+// Nicholas Rahbany
+
+import Sort.*;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    File outputFile;
 
-    public int rand1 = Random();
-    public int rand2 = Random();
-    public int rand3 = Random();
+    public static void main(String[] args) {
+        int[] array1 = buildArray(1000);
+        int[] array2 = buildArray(5000);
+        int[] array3 = buildArray(10000);
+        int[] array4 = buildArray(20000);
+        int[] array5 = buildArray(50000);
+        test(array1);
+    }
 
-    public void test() {
+    private static int[] buildArray(int size) {
+        Random random = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt();
+        }
+        return array;
+    }
 
+    private static void test(int[] array) {
+        int[] resultsInsertion = new int[3]; //[comps, swaps, time]
+        int[] resultsMerge = new int[3];
+        int[] resultsQuick = new int[3];
+        retultsInsertion = insertionSort(array);
+        resultsMerge = mergeSort(array);
+        resultsQuick = quickSort(array);
     }
 
 }
--- Assignment 2/code/readme.txt
+++ Assignment 2/code/readme.txt
@@ -1,1 +1,2 @@
-Used to instruct on how to initalize test
+To run this test, open Main.java and run the test from there.
+The test will automatically create arrays with random numbers of lengths and will output the results to analysis.txt.
