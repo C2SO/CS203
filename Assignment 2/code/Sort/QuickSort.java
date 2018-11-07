c5c948587a81b4ce23c7f8abb3175e4b1669b371
Nicholas Rahbany <nrahbany22@yahoo.com>
2018-11-06 23:17:47 -0500

    Finished comments for QuickSort

--- Assignment 2/code/Sort/QuickSort.java
+++ Assignment 2/code/Sort/QuickSort.java
@@ -9,19 +9,25 @@ package Sort;
 
 public class QuickSort {
 
-    boolean overflow = false;
+    public long comparisons; // Counts the comparisons between two values
+    public long swaps; // Counts the switches between two values in the array
 
-    public long comparisons;
-    public long swaps;
+    /*************************************************/
+    /* Method: QuickSort                             */
+    /* Purpose: Empty constuctor of QuickSort object */
+    /*************************************************/
+    public QuickSort() {}
 
-    public QuickSort() {
-    }
-
-    /*
-     * This function takes last element as pivot, places the pivot element at its
-     * correct position in sorted array, and places all smaller (smaller than pivot)
-     * to left of pivot and all greater elements to right of pivot
-     */
+    /********************************************************************/
+    /* Method: partition                                                */
+    /* Purpose: This function takes last element as pivot, places       */
+    /*     the pivot element at its correct position in sorted array,   */
+    /*     and places all smaller (smaller than pivot) to left of       */
+    /*     pivot and all greater elements to right of pivot             */
+    /* Parameters: int[] - Array that will be partitioned               */
+    /*             int low - Low index of array                         */
+    /*             int high - High index of array                       */
+    /********************************************************************/
     int partition(int arr[], int low, int high) {
         int pivot = arr[high];
         int i = (low - 1); // index of smaller element
@@ -48,10 +54,13 @@ public class QuickSort {
         return i + 1;
     }
 
-    /*
-     * The main function that implements QuickSort() arr[] --> Array to be sorted,
-     * low --> Starting index, high --> Ending index
-     */
+    /*************************************************/
+    /* Method: sort                                  */
+    /* Purpose: Sorts the array                      */
+    /* Parameters: int[] - Array that will be sorted */
+    /*             int low - Low index of array      */
+    /*             int high - High index of array    */
+    /*************************************************/
     void sort(int arr[], int low, int high) {
         try {
             if (low < high) {
@@ -65,25 +74,32 @@ public class QuickSort {
                 sort(arr, low, pi - 1);
                 sort(arr, pi + 1, high);
             }
-        } catch (StackOverflowError e) {
+        } catch (StackOverflowError e) { // If there is a StackOverflowError
             this.overflow = true;
         }
     }
 
-    // Driver program
+    /***********************************************************/
+    /* Method: start                                           */
+    /* Purpose: Handles the test                               */
+    /* Parameters: int[] - Array that will be sorted           */
+    /* Returns: long[] - Array that contains the test results  */
+    /***********************************************************/
     public long[] start(int[] arr) {
+
+        boolean overflow = false; // Sets overflow boolean to false
         comparisons = 0;
         swaps = 0;
-        int n = arr.length;
+        int n = arr.length; // Set as array length
 
-        long startTime = System.nanoTime();
-        sort(arr, 0, n - 1);
-        long endTime = System.nanoTime();
-        long elapsedTime = endTime - startTime;
-        long[] results = { comparisons, swaps, elapsedTime };
-        if (overflow) {
+        long startTime = System.nanoTime(); // Starts timing the sort
+        sort(arr, 0, n - 1); // Sort the array
+        long endTime = System.nanoTime(); // Stops timing the sort
+        long elapsedTime = endTime - startTime; // Calculates the total elapsed time
+        long[] results = { comparisons, swaps, elapsedTime }; // Stors results in array
+        if (overflow) { // If there was an overflow error, then print this error
             System.out.print("Stack Overflow Exception in Quick Sort");
         }
-        return results;
+        return results; // Returns results
     }
 }
