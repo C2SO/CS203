6903b7043a33dd5d84821042ce7a1d83288c3d2c
Nicholas Rahbany <nrahbany22@yahoo.com>
2018-11-06 23:07:30 -0500

    Added comments for InsertionSort

--- Assignment 2/code/Sort/InsertionSort.java
+++ Assignment 2/code/Sort/InsertionSort.java
@@ -2,26 +2,33 @@
 /* Nicholas Rahbany           */
 /* Login ID: rahb3032         */
 /* CS 203, Fall 2018          */
-/* Programming Assignment 1   */
+/* Programming Assignment 2   */
 /******************************/
 
 package Sort;
 
 public class InsertionSort {
 
-    public long comparisons;
-    public long swaps;
+    public long comparisons; // Comparisons between two integers in the array
+    public long swaps; // Swaps between two items in the array
 
-    public InsertionSort() {
-    }
+    /********************************************************/
+    /* Method: InsertionSort                                */
+    /* Purpose: Empty constructor for InsertionSort object  */
+    /* Parameters:                                          */
+    /********************************************************/
+    public InsertionSort() {}
 
-    /* Function to sort array using insertion sort */
+    /*********************************************/
+    /* Method: sort                              */
+    /* Purpose: Sorts array                      */
+    /* Parameters: int[] - Array of integers     */
+    /*********************************************/
     void sort(int arr[]) {
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
+        int n = arr.length; // Used as array length
+        for (int i = 1; i < n; ++i) { // For every integer in the array
             int key = arr[i];
             int j = i - 1;
-
             /*
              * Move elements of arr[0..i-1], that are greater than key, to one position
              * ahead of their current position
@@ -39,18 +46,20 @@ public class InsertionSort {
         }
     }
 
-    // Driver method
+    /*********************************************/
+    /* Method: start                             */
+    /* Purpose: Method that handles the test     */
+    /* Parameters: int[] - Array of integers     */
+    /* Returns: long[] - Results of the test     */
+    /*********************************************/
     public long[] start(int[] arr) {
         comparisons = 0;
         swaps = 0;
-        long startTime = System.nanoTime();
-
-        sort(arr);
-
-        long endTime = System.nanoTime();
-        long elapsedTime = endTime - startTime;
-
-        long[] result = { comparisons, swaps, elapsedTime };
+        long startTime = System.nanoTime(); // Starts timing the system
+        sort(arr); // Sorts the array
+        long endTime = System.nanoTime(); //  Stops timing the system
+        long elapsedTime = endTime - startTime; // Calculates the elapsed time of the sort
+        long[] result = { comparisons, swaps, elapsedTime }; // Stores results in an array
         return result;
     }
 }
